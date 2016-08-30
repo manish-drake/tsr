b8b7188b3994fdcad7706846153de870e6b8317b
Gurpreet Singh <Gurpreet Singh>
2016-08-30 18:19:46 +0530

    testcommit

--- www/js/app.js
+++ www/js/app.js
@@ -17,6 +17,10 @@ angular.module('App', ['ionic'])
     $urlRouterProvider.otherwise('/uatOutSection');
   })
 
+
+
+  
+
   .run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
       if (window.cordova && window.cordova.plugins.Keyboard) {
