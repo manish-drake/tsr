7e24142051537f16e3732d38f40a8405839de0fd
Manish Verma <Manish Verma>
2016-11-23 17:42:06 +0530

    removed rest of the code

--- www/js/indexController.js
+++
@@ -1,4 +0,0 @@
-angular.module('App')
-    .controller('IndexController', function ($scope, $http, $ionicLoading, $ionicPopover,$broadcast) {
-
-    });
--- www/views/modal/connection/connectionController.js
+++
@@ -1,6 +0,0 @@
-angular.module('App')
-  .controller('ConnectionController', function ($scope, $rootScope) {
-    
-    
-});
-
