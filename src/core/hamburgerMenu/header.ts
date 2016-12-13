bb99eaf447f6ad66f1144669fb0e00de1aa7f246
Manish Verma <Manish Verma>
2016-12-13 21:14:48 +0530

    rename hamburger menu types

---
+++ src/core/hamburgerMenu/header.ts
@@ -0,0 +1,23 @@
+import { Group } from './group'
+export class Header{
+    
+    private _Name : string;
+    public get Name() : string {
+        return this._Name;
+    }
+    public set Name(v : string) {
+        this._Name = v;
+    }
+    
+    private _Groups : string[];
+    public get Groups() : string[] {
+        if(!this._Groups)
+            this._Groups = [];
+        return this._Groups;
+    }
+    public set Groups(v : string[]) {
+        this._Groups = v;
+    }
+    
+    
+}
