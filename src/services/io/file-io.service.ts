56379adf64c0e48b700b9b25c7ab5f5e4e840f93
gurpreetdrake <gurpreet.drake@hotmail.com>
2016-12-19 13:12:37 +0530

    unused imports and var commented

--- src/services/io/file-io.service.ts
+++ src/services/io/file-io.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core'
-import { File } from './File'
+// import { File } from './File'
 import { Folder } from './Folder'
 import { FileFactory } from './file-factory'
-import { Observable } from 'Rxjs'
+// import { Observable } from 'Rxjs'
 
 
 @Injectable()
 export class FileIOService {
     getFolder(fullName: string) {
-        var folder = new Folder();
+        // var folder = new Folder();
         var fileFactory = new FileFactory();
         fileFactory.saveFile("file:/storage/emulated/0/DCIM/rootFolder/File", "abc.xml", "ajaja");      
     }
