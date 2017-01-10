c2f573e0384315b9f5d956d7868089ffcb19d9af
VivekDrake <ms.drake@outlook.com>
2017-01-10 19:00:16 +0530

    run to master

--- src/comps/tests/detail-vehicles/detail-vehicles.ts
+++ src/comps/tests/detail-vehicles/detail-vehicles.ts
@@ -1,8 +1,7 @@
 import { Component, Output, EventEmitter } from '@angular/core';
-import { Http } from '@angular/http';
 import { FileFactory } from '../../../services/io/file-factory';
+import { Master } from '../../../services/test-set/master.service'
 import { MoreActionsPopover } from '../../../pages/moreactions/moreactions';
-
 /*
   Generated class for the DetailVehicles page.
 
@@ -25,7 +24,7 @@ export class DetailVehiclesComp {
     { cx: 525, cy: 525, r: 0 }
   ];
 
-  constructor(private http: Http, private fileFactory: FileFactory, private moreaction: MoreActionsPopover) { }
+  constructor(private fileFactory: FileFactory, private master: Master, private moreaction: MoreActionsPopover) { }
 
   navigate(ev) {
     this.onNavigate.emit(ev);
@@ -34,76 +33,81 @@ export class DetailVehiclesComp {
   resultsobjects = [];
   onRun() {
     this.isPlay = !this.isPlay;
-    if (this.isPlay) {
-      setTimeout(() => {
-      this.http.get('assets/jsonfiles/data.json')
-        .map((res) => res.json())
-        .subscribe(data => {
-          if (!MoreActionsPopover.isRunAllenabled) {
-            this.resultsobjects = [];
-            var item = data.response.data.results[this.selectedVehicleIndex];
-            this.resultsobjects.push(item);
-          }
-          else {
-            this.resultsobjects = [];
-            this.resultsobjects = data.response.data.results;
-          }
-        }, (rej) => { console.error("Could not load local data", rej) });
-        this.isPlay = false;
-      }, 2000);      
-    }    
-  }
-  isTargetposAvailable: boolean = false;
 
-  selectAv() {
-    this.isTargetposAvailable = !this.isTargetposAvailable;
-  }
-
-  ngAfterViewInit() {
-    this.onResize(event);
-
-    this.http.get('assets/jsonfiles/data.json')
-      .map((res) => res.json())
+    this.master.scanTest()
       .subscribe(data => {
-        var results = data.response.data.results;
-        results.forEach(element => {
-          var item = { "item": "1" }
-          this.resultsobjects.push(item);
-        });
+        console.log(data);
+        this.resultsobjects = [];
+        if (!MoreActionsPopover.isRunAllenabled) {              //code for run selected vechile  
+          var items = data.response.data.results;
+          items.forEach((element, index) => {
+            if (index == this.selectedVehicleIndex) {
+              var itemIndex = items[this.selectedVehicleIndex];
+              this.resultsobjects.push(itemIndex);
+              items.splice(-1, 1);
+            }
+            var item = {};
+            this.resultsobjects.push(item);
+          })
+        }
+        else {                                                  //code for run all
+          this.resultsobjects = data.response.data.results;
+        }
       }, (rej) => { console.error("Could not load local data", rej) });
-  }
+    this.isPlay = false;
+ 
+}
+isTargetposAvailable: boolean = false;
 
-  isPortrait: boolean = true;
-  onResize(event) {
-    if (window.innerHeight > window.innerWidth) {
-      this.isPortrait = true;
-    }
-    else if (window.innerHeight < window.innerWidth) {
-      this.isPortrait = false;
-    }
-  }
+selectAv() {
+  this.isTargetposAvailable = !this.isTargetposAvailable;
+}
 
-  getY(lat, lon) {
-    var containerMid = 525;
-    var radius = 470;
-    var objectsize = 30;
-    var rad2deg = Math.PI / 180;
-    var lony = (lat * (2 * radius) / 180)
-    return containerMid - lony * (Math.cos(lon * rad2deg)) - (objectsize / 2);
-  }
+ngAfterViewInit() {
+  this.onResize(event);
 
-  getX(lat, lon) {
-    var containerMid = 525;
-    var radius = 470;
-    var objectsize = 30;
-    var rad2deg = Math.PI / 180;
-    var latx = radius * Math.sin(lon * rad2deg);
-    return containerMid + latx - (objectsize / 2);
-  }
+  // this.http.get('assets/jsonfiles/data.json')
+  //   .map((res) => res.json())
+  //   .subscribe(data => {
+  //     var results = data.response.data.results;
+  //     results.forEach(element => {
+  //       var item = { "item": "1" }
+  //       this.resultsobjects.push(item);
+  //     });
+  //   }, (rej) => { console.error("Could not load local data", rej) });
+}
 
-  selectedVehicleIndex = 0;
-
-  onVehicleClick(index) {
-    this.selectedVehicleIndex = index;
+isPortrait: boolean = true;
+onResize(event) {
+  if (window.innerHeight > window.innerWidth) {
+    this.isPortrait = true;
   }
+  else if (window.innerHeight < window.innerWidth) {
+    this.isPortrait = false;
+  }
+}
+
+getY(lat, lon) {
+  var containerMid = 525;
+  var radius = 470;
+  var objectsize = 30;
+  var rad2deg = Math.PI / 180;
+  var lony = (lat * (2 * radius) / 180)
+  return containerMid - lony * (Math.cos(lon * rad2deg)) - (objectsize / 2);
+}
+
+getX(lat, lon) {
+  var containerMid = 525;
+  var radius = 470;
+  var objectsize = 30;
+  var rad2deg = Math.PI / 180;
+  var latx = radius * Math.sin(lon * rad2deg);
+  return containerMid + latx - (objectsize / 2);
+}
+
+selectedVehicleIndex = 0;
+
+onVehicleClick(index) {
+  this.selectedVehicleIndex = index;
+}
 }
--- src/services/test-set/master.service.ts
+++ src/services/test-set/master.service.ts
@@ -1,14 +1,35 @@
 import { Injectable } from '@angular/core'
 import { Dictionary } from '../../common/dictionary'
+import { Observable } from 'Rxjs'
+import { Http } from '@angular/http';
 
 @Injectable()
-export class Master{
-    
-    constructor() {
+export class Master {
+
+    constructor(private http: Http) {
 
     }
 
-    runTest(testName: string, args:Dictionary<string, string>): string{
+    scanTest(): Observable<any> {
+        console.log('posting');
+        return this.http.post('http://claganga.homeip.net/cgi-bin/UATCGIServer',
+            {
+                "request":
+                {
+                    "version": 2, "message": 8196, "params":
+                    {
+
+                    }
+                }
+            })
+            .map((res) => {
+                console.log(res);
+
+                return res.json();
+            })
+    }
+
+    runTest(testName: string, args: Dictionary<string, string>): string {
         return "";
     }
 }
