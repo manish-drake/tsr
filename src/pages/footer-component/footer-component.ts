3009b4e09e832f9735a6d8559645c02b170dc228
gurpreetdrake <gurpreet.drake@hotmail.com>
2017-01-19 16:14:41 +0530

    battery status implemented

--- src/pages/footer-component/footer-component.ts
+++ src/pages/footer-component/footer-component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BatteryStatus, StatusObject } from 'ionic-native';
 
 @Component({
   selector: 'footer-component',
@@ -7,17 +8,30 @@ import { NavController } from 'ionic-angular';
 })
 export class FooterComponent {
 
-  constructor(public navCtrl: NavController) {
-    
-  }
+  constructor(public navCtrl: NavController) { }
 
-  displaydate:any;
+  displaydate: any;
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     this.displaydate = Date();
     setInterval(() => {
       this.displaydate = Date();
     }, 1000);
+
+    BatteryStatus.onChange().subscribe(
+      (status: StatusObject) => {
+        this.onBatteryStatus(status);
+      }
+    );
   }
+
+  batteryLevel: any = "100%";
+
+  onBatteryStatus(status: StatusObject) {
+    this.batteryLevel = status.level + "%";
+    if (status.isPlugged) {
+    }
+  }
+
 
 }
