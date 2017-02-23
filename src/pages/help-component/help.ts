cd07aa1e8f83d3dacccd847534fefb480d7a4daf
gurpreetdrake <gurpreet.drake@hotmail.com>
2017-02-23 16:43:54 +0530

    theme changes

--- src/pages/help-component/help.ts
+++ src/pages/help-component/help.ts
@@ -1,11 +1,6 @@
 import { Component } from '@angular/core';
 import { ViewController } from 'ionic-angular';
-/*
-  Generated class for the Help page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-help',
   templateUrl: 'help.html'
