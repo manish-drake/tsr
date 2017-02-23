04ee1647bccc967e33235e316110d01cd58784d1
Gurpreet Tamber <tester@drake.in>
2017-02-23 18:01:58 +0530

    UI corrections

--- src/services/language/language-service.ts
+++ src/services/language/language-service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/Rx';
-import { TranslateService } from 'ng2-translate';
+import { TranslateService } from 'ng2-translate/ng2-translate';
 import { LocalStorage } from '../../services/storage/local-storage'
 
 @Injectable()
