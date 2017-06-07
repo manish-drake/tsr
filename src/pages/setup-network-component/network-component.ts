ceffff4d3d2c0fb8a01a119fe38deb02cb1f4f13
gurpreetdrake <gurpreet.drake@hotmail.com>
2017-06-07 15:30:13 +0530

    connnection scenario UI

--- src/pages/setup-network-component/network-component.ts
+++ src/pages/setup-network-component/network-component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { DevicesService } from '../../services/test-set/devices.service';
+import { ConnectionService } from '../../services/test-set/connection.service';
 
 @Component({
   selector: 'page-network-component',
@@ -9,19 +11,27 @@ export class SetupNetworkComp {
 
   constructor(
     public _router: Router,
-    private route: ActivatedRoute
-  ) {}
+    private route: ActivatedRoute,
+    private _svcDevices: DevicesService,
+    private _svcConnection: ConnectionService
+  ) { }
 
   parent: string;
+  connectedDeviceName: string;
 
   ngOnInit() {
     this.route.params.subscribe(param => {
       this.parent = (param as any).parent;
     });
+    this._svcDevices.getconnectedDevice().subscribe(val => this.connectedDeviceName = val.name);
   }
 
   onGoBack() {
     this._router.navigate(['setup', this.parent]);
+  }
+
+  onScanDevices() {
+    this._svcConnection.ScanDevices();
   }
 
 }
