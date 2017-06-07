8d56030642cdc0fdb23151d83f546e957c2784ba
gurpreetdrake <gurpreet.drake@hotmail.com>
2017-06-07 11:34:58 +0530

    zeromq installed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 
 import { HomeService } from '../../services/ui/home.service'
 import { LanguageService } from '../../services/language/language-service';
+
+// import {z} from 'zeromq'
+// declare var zmq: any;
 
 @Component({
   selector: 'page-home',
@@ -11,6 +14,7 @@ import { LanguageService } from '../../services/language/language-service';
 export class HomePage implements OnInit {
 
   constructor(
+    public platform: Platform,
     public navCtrl: NavController,
     private _svcHome: HomeService,
     private _srvLanguage: LanguageService) { }
@@ -26,4 +30,15 @@ export class HomePage implements OnInit {
     this._svcHome.FooterUpdated.subscribe(e => this.footerData = e);
     this._srvLanguage.getSavedLanguage();
   }
+
+  ionViewDidLoad() {
+    this.platform.ready().then(() => {
+      // var sockt = z.socket('rep');
+      // sockt.connect('tcp://192.168.1.104:6000');
+      // sockt.on('message', function (msg) {
+      //   alert('message');
+      // });
+    });
+  }
+
 }
